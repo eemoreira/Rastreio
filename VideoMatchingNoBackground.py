bfd2b39c7bc3427befb221c23d621e780887aca0
emoreira <theeduardogi682@gmail.com>
2024-11-18 17:54:49 -0300

    frames com matching

---
+++ VideoMatchingNoBackground.py
@@ -0,0 +1,34 @@
+import cv2 as cv
+import os
+
+# best 2 mathods from previous part
+methods = ['TM_CCOEFF_NORMED', 'TM_CCORR']
+
+template = cv.imread("MonsterTemplate.png", cv.IMREAD_GRAYSCALE)
+w, h = template.shape[::-1]
+frames_dir = "frames"
+output_dir = "VideoFrameMatchingNoBackground"
+
+def getMatchings(img, name):
+    for meth in methods:
+        method = getattr(cv, meth)
+
+        res = cv.matchTemplate(img, template, method)
+        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+
+        top_left = max_loc
+        bottom_right = (top_left[0] + w, top_left[1] + h)
+
+        matched_img = cv.cvtColor(img, cv.COLOR_GRAY2BGR)  # Convert to BGR for colored rectangle
+        cv.rectangle(matched_img, top_left, bottom_right, (0, 255, 0), 2)
+
+        output_path = os.path.join(output_dir, f"{meth}/{name}_{meth}.jpg")
+        cv.imwrite(output_path, matched_img)
+
+for filename in os.listdir(frames_dir):
+    if filename.endswith(".jpg"):
+        print(filename)
+        img = cv.imread(f'frames/{filename}', cv.IMREAD_GRAYSCALE)
+        getMatchings(img, filename[:-4])
+    else:
+        exit(1)
